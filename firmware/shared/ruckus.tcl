8af02e20abe17e36710059e3b525b53c01350de9
Larry Ruckman <ruckman@slac.stanford.edu>
2022-02-01 09:41:57 -0800

    adding Lili's code

--- firmware/shared/ruckus.tcl
+++ firmware/shared/ruckus.tcl
@@ -6,7 +6,15 @@ loadRuckusTcl $::env(TOP_DIR)/submodules/surf
 loadRuckusTcl $::env(TOP_DIR)/submodules/axi-soc-ultra-plus-core/hardware/XilinxZcu208
 
 # Load RTL code
-loadSource -dir "$::DIR_PATH/rtl"
+loadSource -dir  "$::DIR_PATH/rtl"
+loadSource -path "$::DIR_PATH/simulink/netlist/dspcore.dcp"
 
 # Load IP cores
 loadIpCore -dir "$::DIR_PATH/ip"
+
+# Load SYSGEN .ZIP output file
+if { [get_ips analysis_0] eq ""  } {
+   loadZipIpCore  -repo_path $::env(IP_REPO) -dir "$::DIR_PATH/simulink/netlist/ip"
+   create_ip -name analysis -vendor SLAC -library SysGen -version 1.0 -module_name analysis_0
+   set_property target_language Verilog [current_project]
+}
