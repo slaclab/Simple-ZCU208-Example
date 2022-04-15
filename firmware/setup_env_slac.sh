3bdb130fe47f44cc12eb73aa6fa2ea20e5316f7c
Larry Ruckman <ruckman@slac.stanford.edu>
2022-04-15 10:16:00 -0700

    updating scripts

---
+++ firmware/setup_env_slac.sh
@@ -0,0 +1,5 @@
+# Setup the Xilinx software and licensing
+source /afs/slac.stanford.edu/g/reseng/vol35/xilinx/2021.2/Vivado/2021.2/settings64.sh
+
+# Setup the Matlab software and licensing
+source /afs/slac.stanford.edu/g/reseng/vol35/xilinx/2021.2/Vivado/2021.2/.settings64-sysgen.sh
--- firmware/vivado_setup.sh
+++
@@ -1,2 +0,0 @@
-# Setup the Xilinx software and licensing
-source /afs/slac.stanford.edu/g/reseng/xilinx/2021.2/Vivado/2021.2/settings64.sh
