b6c8601ed177394c017f1199b9b31a2dc50d99b8
Larry Ruckman <ruckman@slac.stanford.edu>
2024-07-05 12:11:14 -0700

    updating submodules

--- firmware/targets/shared_version.mk
+++ firmware/targets/shared_version.mk
@@ -1,5 +1,5 @@
-# Define Firmware Version: v1.7.0.0
-export PRJ_VERSION = 0x01070000
+# Define Firmware Version: v1.8.0.0
+export PRJ_VERSION = 0x01080000
 
 # Include .XSA in image dir
 export GEN_XSA_IMAGE = 1
