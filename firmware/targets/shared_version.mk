5ad44cff45174724b77d2c6dbb86f8363bb72ee2
Larry Ruckman <ruckman@slac.stanford.edu>
2025-01-08 20:39:04 +0000

    upgrading to axi-soc-ultra-plus-core@4.4.0

--- firmware/targets/shared_version.mk
+++ firmware/targets/shared_version.mk
@@ -1,5 +1,5 @@
-# Define Firmware Version: v1.8.0.0
-export PRJ_VERSION = 0x01080000
+# Define Firmware Version: v2.0.0.0
+export PRJ_VERSION = 0x02000000
 
 # Include .XSA in image dir
 export GEN_XSA_IMAGE = 1
