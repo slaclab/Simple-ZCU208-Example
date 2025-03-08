d73ef9fd76b80ca4626fd3b1b2d1aad78aa9f055
Larry Ruckman <ruckman@slac.stanford.edu>
2025-03-08 15:51:51 -0800

    upgrading to axi-soc-ultra-plus-core@5.0.1

--- firmware/targets/SimpleZcu208Example/hdl/SimpleZcu208Example.vhd
+++ firmware/targets/SimpleZcu208Example/hdl/SimpleZcu208Example.vhd
@@ -45,8 +45,12 @@ entity SimpleZcu208Example is
       dacClkN   : in    sl;
       dacP      : out   slv(7 downto 0);
       dacN      : out   slv(7 downto 0);
-      sysRefP   : in    sl;
-      sysRefN   : in    sl;
+      sysRefP   : in  sl;
+      sysRefN   : in  sl;
+      plClkP    : in  sl;
+      plClkN    : in  sl;
+      plSysRefP : in  sl;
+      plSysRefN : in  sl;
       -- SYSMON Ports
       vPIn      : in    sl;
       vNIn      : in    sl);
@@ -89,30 +93,6 @@ architecture top_level of SimpleZcu208Example is
 
 begin
 
-   ------------------------------
-   -- User's AXI-Lite Clock/Reset
-   ------------------------------
-   U_axilClk : entity surf.ClockManagerUltraScale
-      generic map(
-         TPD_G             => TPD_G,
-         TYPE_G            => "PLL",
-         INPUT_BUFG_G      => false,
-         FB_BUFG_G         => true,
-         RST_IN_POLARITY_G => '1',
-         NUM_CLOCKS_G      => 1,
-         -- MMCM attributes
-         CLKIN_PERIOD_G    => 4.0,      -- 250 MHz
-         CLKFBOUT_MULT_G   => 4,        -- 1.0GHz = 4 x 250 MHz
-         CLKOUT0_DIVIDE_G  => 10)       -- 100MHz = 1.0GHz/10
-      port map(
-         -- Clock Input
-         clkIn     => dmaClk,
-         rstIn     => dmaRst,
-         -- Clock Outputs
-         clkOut(0) => axilClk,
-         -- Reset Outputs
-         rstOut(0) => axilRst);
-
    -----------------------
    -- Common Platform Core
    -----------------------
@@ -125,10 +105,12 @@ begin
       port map (
          ------------------------
          --  Top Level Interfaces
-         ------------------------
          -- DSP Clock and Reset Monitoring
          dspClk          => dspClk,
          dspRst          => dspRst,
+         -- AUX Clock and Reset
+         auxClk          => axilClk,
+         auxRst          => axilRst,
          -- DMA Interfaces  (dmaClk domain)
          dmaClk          => dmaClk,
          dmaRst          => dmaRst,
@@ -215,6 +197,10 @@ begin
          dacN            => dacN,
          sysRefP         => sysRefP,
          sysRefN         => sysRefN,
+         plClkP          => plClkP,
+         plClkN          => plClkN,
+         plSysRefP       => plSysRefP,
+         plSysRefN       => plSysRefN,
          -- ADC/DAC Interface (dspClk domain)
          dspClk          => dspClk,
          dspRst          => dspRst,
